7a74e413fc7cb4bc6a6c89fe72181a4bbb3a22fa
Leonardo Larrosa <llarrosasanchez@teco.com.ar>
2018-12-16 15:53:23 -0300

    ADD basic structure for Title component

---
+++ src/components/Title/Title.js
@@ -0,0 +1,19 @@
+export default {
+  name: 'title',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
---
+++ src/components/Title/index.vue
@@ -0,0 +1,4 @@
+<template src="./Title.html"></template>
+<script src="./Title.js"></script>
+<style src="./Title.scss" scoped lang="scss"></style>
+
