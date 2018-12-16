92b6909edb027e091d015b9de9d382b9f594962f
Leonardo Larrosa <llarrosasanchez@teco.com.ar>
2018-12-16 15:07:40 -0300

    ADD basic structure for List component

--- example/src/App.vue
+++ example/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <tag-component text="example text"></tag-component>
     <btn />
+    <List/>
   </div>
 </template>
 
 <script>
-import { tagComponent, btn } from '../../src/index'
+import { tagComponent, btn, List } from '../../src/index'
+
 export default {
   name: 'app',
   components: {
     tagComponent,
-    btn
+    btn,
+    List
   }
 }
 </script>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import tagComponent from './components/tag-component/tag-component'
 import btn from './components/button/btn'
+import List from './components/List'
 
 export {
   tagComponent,
-  btn
+  btn,
+  List
 }
